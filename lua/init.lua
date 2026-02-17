af9e1ce253bb81f2eaf881f34ed3a33a310809c6
Ksenia Busquet <ksenia.busquet@gmail.com>
2026-02-16 22:16:34 -0300

    Kikstart configs

--- lua/init.lua
+++
@@ -1,7 +0,0 @@
-rvim.g.mapleader = " "
-vim.g.maplocalleader = " "
-vim.opt.clipboard = "unnamedplus"
-require("config.options")
-require("config.lazy")
-require("config.lsp")
-require("config.cmp")
